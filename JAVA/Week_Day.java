53bcb180f8c005d8a73d40376e616bf752ffe9ac
dvlpr_kaushik <freakyboyftw@gmail.com>
2023-02-14 21:15:35 +0530

    code

--- JAVA/Week_Day.java
+++ JAVA/Week_Day.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 
 public class Week_Day {
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter nth day of the week : ");
         int _week_day = scan.nextInt();
 
-        switch (_week_day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            default:
-                System.out.println("Sunday");
-                break;
+        if (_week_day > 7) {
+            System.out.println("<--Does not exist-->");
+        } else {
+
+            switch (_week_day) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                default:
+                    System.out.println("Sunday");
+                    break;
+            }
         }
     }
 }
